43ef337c7e9a8ba96a595d93c45a6db9ca5e1055
Habakuk Beneke <hbeneke@proton.me>
2026-07-01 12:28:26 +0200

    chore(config): remove unused siteUrl

--- apps/web/site.config.ts
+++ apps/web/site.config.ts
@@ -1,6 +1,5 @@
 export const siteConfig = {
   nickname: "Habakuk Beneke",
-  siteUrl: "https://equero.dev",
   siteCreationYear: "2024",
   get copyrightYear() {
     return `${this.siteCreationYear}-${new Date().getFullYear().toString().slice(-2)}`;
